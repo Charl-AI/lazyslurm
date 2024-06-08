a33f0e7dec28460f513f9933f8c27a0ad8e404f4
C Jones <cj1917@ic.ac.uk>
2024-06-08 17:23:34 +0100

    monitoring working

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,204 @@
+use ratatui::widgets::ListState;
+use std::{io::BufRead, process::Command};
+
 pub enum Action {
     Quit,
     Tick,
     Down,
     Up,
+    Home,
+    End,
+    PageDown,
+    PageUp,
     //ToggleFocus,
+    //ConfirmAction
+    //CancelAction
     //Attach,
     //Kill,
 }
 
+pub struct Job {
+    pub job_id: String,
+    pub array_id: String,
+    pub array_step: String,
+    pub name: String,
+    pub state: String,
+    pub state_compact: String,
+    pub reason: String,
+    pub user: String,
+    pub timeused: String,
+    pub starttime: String,
+    pub submittime: String,
+    pub tres: String,
+    pub partition: String,
+    pub nodelist: String,
+}
+
+fn get_jobs() -> Vec<Job> {
+    let output_separator = "###";
+    let fields = [
+        "jobid",
+        "name",
+        "state",
+        "username",
+        "timeused",
+        "submittime",
+        "starttime",
+        "tres",
+        "partition",
+        "nodelist",
+        "statecompact",
+        "reason",
+        "ArrayJobID",
+        "ArrayTaskID",
+    ];
+    let output_format = fields
+        .map(|s| s.to_owned() + ":" + output_separator)
+        .join(",");
+
+    let jobs: Vec<Job> = Command::new("squeue")
+        .arg("--array")
+        .arg("--noheader")
+        .arg("--Format")
+        .arg(&output_format)
+        .output()
+        .expect("failed to execute squeue")
+        .stdout
+        .lines()
+        .map(|l| l.unwrap().trim().to_string())
+        .filter_map(|l| {
+            let parts: Vec<_> = l.split(output_separator).collect();
+
+            if parts.len() != fields.len() + 1 {
+                return None;
+            }
+
+            let id = parts[0];
+            let name = parts[1];
+            let state = parts[2];
+            let user = parts[3];
+            let timeused = parts[4];
+            let submittime = parts[5];
+            let starttime = parts[6];
+            let tres = parts[7];
+            let partition = parts[8];
+            let nodelist = parts[9];
+            let state_compact = parts[10];
+            let reason = parts[11];
+
+            let array_job_id = parts[12];
+            let array_task_id = parts[13];
+
+            Some(Job {
+                job_id: id.to_owned(),
+                name: name.to_owned(),
+                state: state.to_owned(),
+                state_compact: state_compact.to_owned(),
+                user: user.to_owned(),
+                timeused: timeused.to_owned(),
+                starttime: starttime.to_owned(),
+                submittime: submittime.to_owned(),
+                tres: tres.to_owned(),
+                partition: partition.to_owned(),
+                nodelist: nodelist.to_owned(),
+                array_id: array_job_id.to_owned(),
+                reason: reason.to_owned(),
+                array_step: array_task_id.to_owned(),
+            })
+        })
+        .collect();
+    jobs
+}
+
 pub struct App {
     pub should_quit: bool,
-    //jobs: Vec<Job>,
+    pub jobs: Vec<Job>,
+    pub state: ListState,
 }
 
 impl App {
     pub fn new() -> Self {
-        App { should_quit: false }
+        let mut state = ListState::default();
+        state.select(Some(0));
+        App {
+            should_quit: false,
+            jobs: get_jobs(),
+            state,
+        }
     }
 
     pub fn update(&mut self, action: Action) -> () {
         match action {
             Action::Quit => self.should_quit = true,
+            Action::Tick => self.jobs = get_jobs(),
+            Action::Up => self.previous(),
+            Action::Down => self.next(),
+            Action::Home => self.home(),
+            Action::End => self.end(),
+            Action::PageDown => self.down_5(),
+            Action::PageUp => self.up_5(),
             _ => (),
         }
+    }
+
+    pub fn next(&mut self) -> () {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.jobs.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.jobs.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn down_5(&mut self) -> () {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.jobs.len() - 5 {
+                    self.jobs.len() - 1
+                } else {
+                    i + 5
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+    pub fn up_5(&mut self) -> () {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i <= 5 {
+                    0
+                } else {
+                    i - 5
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn home(&mut self) -> () {
+        self.state.select(Some(0))
+    }
+    pub fn end(&mut self) -> () {
+        self.state.select(Some(self.jobs.len() - 1))
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,201 @@
-use ratatui::{terminal::Frame, widgets::Paragraph};
+use ratatui::{
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    terminal::Frame,
+    text::{Line, Span, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+};
 
-use crate::app::App;
+use crate::app::{App, Job};
+
+fn get_short_jobs_list(jobs: &Vec<Job>) -> Vec<ListItem> {
+    jobs.iter()
+        .map(|j| {
+            ListItem::new(Line::from(vec![
+                Span::styled(
+                    format!(" {:<max$.max$} ", j.state_compact, max = 2),
+                    Style::default(),
+                ),
+                Span::styled(
+                    format!(" {:<max$.max$} ", j.job_id, max = 6),
+                    Style::default().fg(Color::Yellow),
+                ),
+                Span::styled(
+                    format!(" {:<max$.max$} ", j.partition, max = 11),
+                    Style::default().fg(Color::Green),
+                ),
+                Span::styled(
+                    format!(" {:<max$.max$} ", j.user, max = 6),
+                    Style::default().fg(Color::Blue),
+                ),
+                Span::styled(
+                    format!(" {:<max$.max$} ", j.timeused, max = 11),
+                    Style::default().fg(Color::Cyan),
+                ),
+                Span::styled(
+                    format!(" {:<max$.max$}", j.name, max = 100),
+                    Style::default().fg(Color::LightRed),
+                ),
+            ]))
+        })
+        .collect()
+}
+
+fn get_job_details(job: &Job) -> Paragraph {
+    let max_width = 11;
+    let status = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "Status", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(
+            format!("{} ({})", job.state, job.state_compact),
+            Style::default(),
+        ),
+    ]);
+    let user = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "User", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.user, Style::default()),
+    ]);
+    let reason = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "Reason", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.reason, Style::default()),
+    ]);
+    let jobid = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "JobID", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.job_id, Style::default()),
+    ]);
+    let arrayid = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "ArrayID", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.array_id, Style::default()),
+    ]);
+    let array_step = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "ArrayStep", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.array_step, Style::default()),
+    ]);
+    let partition = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "Partition", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.partition, Style::default()),
+    ]);
+    let nodelist = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "NodeList", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.nodelist, Style::default()),
+    ]);
+    let submittime = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "SubmitTime", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.submittime, Style::default()),
+    ]);
+    let starttime = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "StartTime", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.starttime, Style::default()),
+    ]);
+    let timeused = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "RunningTime", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.timeused, Style::default()),
+    ]);
+    let tres = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "TRES", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.tres, Style::default()),
+    ]);
+    let command = Line::from(vec![
+        Span::styled(
+            format!("{:<max$.max$}", "Command", max = max_width),
+            Style::default().fg(Color::Yellow),
+        ),
+        Span::raw(" "),
+        Span::styled(&job.name, Style::default()),
+    ]);
+
+    let text = Text::from(vec![
+        status, user, reason, jobid, arrayid, array_step, partition, nodelist, submittime,
+        starttime, timeused, tres, command,
+    ]);
+
+    Paragraph::new(text)
+}
 
 pub fn draw(f: &mut Frame, app: &mut App) {
-    let area = f.size();
-    f.render_widget(Paragraph::new("Hello Ratatui! (press 'q' to quit)"), area);
+    let outer_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(1),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .split(f.size());
+
+    let inner_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(outer_layout[1]);
+
+    f.render_widget(
+        Paragraph::new("LazySLURM v0.0 | C Jones | github.com/Charl-AI/lazyslurm"),
+        outer_layout[0],
+    );
+    f.render_stateful_widget(
+        List::new(get_short_jobs_list(&app.jobs))
+            .block(Block::new().borders(Borders::ALL).title_top("Jobs"))
+            .highlight_style(Style::new().add_modifier(Modifier::REVERSED))
+            .repeat_highlight_symbol(true),
+        inner_layout[0],
+        &mut app.state,
+    );
+    let idx = app.state.selected().unwrap();
+    let job = &app.jobs[idx];
+    f.render_widget(
+        get_job_details(&job).block(Block::new().borders(Borders::ALL).title_top("Details")),
+        inner_layout[1],
+    );
+    f.render_widget(
+        Paragraph::new(
+            "q: quit | k: up | j: down | a: attach to job | x: cancel job | <tab>: show only my jobs (toggle)",
+        ),
+        outer_layout[2],
+    );
 }
